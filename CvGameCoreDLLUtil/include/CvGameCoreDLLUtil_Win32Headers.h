09018a88ec63ea1c3117ca887f2e633c2964f35d
icetbr <icetbr@yahoo.com.br>
2024-07-26 14:37:49 -0300

    feat: RCP API (WIP)

--- CvGameCoreDLLUtil/include/CvGameCoreDLLUtil_Win32Headers.h
+++ CvGameCoreDLLUtil/include/CvGameCoreDLLUtil_Win32Headers.h
@@ -4,51 +4,51 @@
 //!
 //!		Basic Win32 headers w/ necessary preprocessor defines.
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#pragma once 
+#pragma once
 #ifndef CVGAMECOREDLLUTIL_WIN32HEADERS_H
 #define CVGAMECOREDLLUTIL_Win32HEADERS_H
 
 //Defines to reduce the amount included by the standard Windows.h
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
-#define NOGDICAPMASKS    
+#define NOGDICAPMASKS
 #define NOVIRTUALKEYCODES
-#define NOWINMESSAGES    
-#define NOWINSTYLES      
-#define NOSYSMETRICS     
-#define NOMENUS          
-#define NOICONS          
-#define NOKEYSTATES      
-#define NOSYSCOMMANDS    
-#define NORASTEROPS      
-#define NOSHOWWINDOW     
-#define OEMRESOURCE      
-#define NOATOM           
-#define NOCLIPBOARD      
+#define NOWINMESSAGES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
 #define NOCOLOR
-#define NOCTLMGR         
-#define NODRAWTEXT       
-#define NOGDI            
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
 #define NOKERNEL
-#define NOUSER                    
-#define NOMB             
-#define NOMEMMGR         
-#define NOMETAFILE       
-#define NOMINMAX         
-#define NOMSG            
-#define NOOPENFILE       
-#define NOSCROLL         
-#define NOSERVICE        
-#define NOSOUND          
-#define NOTEXTMETRIC     
-#define NOWH             
-#define NOWINOFFSETS     
-#define NOCOMM           
-#define NOKANJI          
-#define NOHELP           
-#define NOPROFILER       
-#define NODEFERWINDOWPOS 
-#define NOMCX   
+//#define NOUSER
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
 
 //Now to include Windows.h
 #include <Windows.h>
